d5f3a5457fe9cc8e65dc47a1af5c2edd8b22ceb9
Arian <Arianbod@outlook.com>
2024-04-21 22:52:20 +0300

    step 3 update with making selecting into photo selecting

--- components/3.jsx
+++ components/3.jsx
@@ -1,90 +1,80 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import Data from '@/utils/Data.json';
+import Option1 from '@/assets/stepFour/1.png';
+import Option2 from '@/assets/stepFour/2.png';
+import Option3 from '@/assets/stepFour/3.png';
+import Option4 from '@/assets/stepFour/4.png';
+import Image from 'next/image';
 import { Button } from './ui/button';
 import { Progress } from '@/components/ui/progress';
 
-const Three = () => {
-	const [formData, setFormData] = useState({
-		yearOfMaintenance: '',
-		structuralSystem: '',
-	});
+const photos = [
+	{ id: '1', url: Option1, description: 'Description for photo 1' },
+	{ id: '2', url: Option2, description: 'Description for photo 2' },
+	{ id: '3', url: Option3, description: 'Description for photo 3' },
+	{ id: '4', url: Option4, description: 'Description for photo 4' },
+];
 
-	const handleChange = (e) => {
-		const { name, value } = e.target;
-		setFormData((prevState) => ({
-			...prevState,
-			[name]: value,
-		}));
+const Four = () => {
+	const [selectedPhoto, setSelectedPhoto] = useState('');
+
+	const stepThreeData = Data.steps.find((step) => step.step === 3);
+
+	const handleSelectionChange = (e) => {
+		setSelectedPhoto(e.target.value);
 	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log(formData);
+		console.log('Selected photo:', selectedPhoto);
 	};
 
-	const stepThreeData = Data.steps.find((step) => step.step === 3);
-
 	return (
-		<div className='max-w-4xl mx-auto p-5'>
-			<Progress value={30} />
+		<div className='max-w-4xl mx-auto p-5 flex flex-col gap-4'>
+			<Progress value={40} />
 
 			<h1 className='text-3xl font-bold text-center mb-6'>
 				{stepThreeData.title}
 			</h1>
-			<p className='text-lg mb-6'>{stepThreeData.description}</p>
+			<p className='text-lg mx-auto text-center'>{stepThreeData.description}</p>
 			<form
 				onSubmit={handleSubmit}
-				className='grid grid-cols-1 gap-6'>
-				{stepThreeData.inputs.map((input, index) =>
-					input.type === 'number' ? (
+				className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+				{photos.map((photo) => (
+					<label
+						key={photo.id}
+						className='block'>
+						<input
+							type='radio'
+							name='photoOption'
+							value={photo.id}
+							checked={selectedPhoto === photo.id}
+							onChange={handleSelectionChange}
+							className='hidden'
+						/>
 						<div
-							key={index}
-							className='flex flex-col'>
-							<label className='text-sm font-medium text-gray-700'>
-								{input.label}
-							</label>
-							<input
-								type='number'
-								name={input.label.replace(/\s+/g, '').toLowerCase()}
-								value={formData[input.label.replace(/\s+/g, '').toLowerCase()]}
-								onChange={handleChange}
-								min={1950}
-								max={2024}
-								defaultValue={2010}
-								className='mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500'
+							className={`cursor-pointer p-4 border-2 ${
+								selectedPhoto === photo.id
+									? 'border-blue-500'
+									: 'border-transparent'
+							} rounded-lg`}>
+							<Image
+								src={photo.url}
+								alt={photo.description}
+								width={500} // Specify width
+								height={300} // Specify height to maintain aspect ratio
+								objectFit='cover' // Cover to ensure the image covers the area nicely
+								className='rounded-md'
 							/>
 						</div>
-					) : (
-						<div
-							key={index}
-							className='flex flex-col'>
-							<label className='text-sm font-medium text-gray-700'>
-								{input.label}
-							</label>
-							<select
-								name={input.label.replace(/\s+/g, '').toLowerCase()}
-								value={formData[input.label.replace(/\s+/g, '').toLowerCase()]}
-								onChange={handleChange}
-								className='mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500'>
-								{input.options.map((option) => (
-									<option
-										key={option}
-										value={option}>
-										{option}
-									</option>
-								))}
-							</select>
-						</div>
-					)
-				)}
-				<div className='flex justify-end'>
+					</label>
+				))}
+				<div className='flex justify-center mt-6'>
 					<Link
-						href='/3-2'
+						href='/4'
 						passHref>
-						<Button
-							type='submit'
-							className='inline-flex items-center justify-center px-5 py-2 border border-transparent text-base font-medium rounded-md text-white hover:bg-zinc-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500'>
+						<Button className=' text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition-colors duration-150'>
 							Next Step
 						</Button>
 					</Link>
@@ -94,4 +84,4 @@ const Three = () => {
 	);
 };
 
-export default Three;
+export default Four;
--- components/4.jsx
+++ components/4.jsx
@@ -31,13 +31,13 @@ const Four = () => {
 	};
 
 	return (
-		<div className='max-w-4xl mx-auto p-5'>
+		<div className='max-w-4xl mx-auto p-5 flex flex-col gap-4'>
 			<Progress value={40} />
 
 			<h1 className='text-3xl font-bold text-center mb-6'>
 				{stepFourData.title}
 			</h1>
-			<p className='text-lg mb-6'>{stepFourData.description}</p>
+			<p className='text-lg mx-auto text-center'>{stepFourData.description}</p>
 			<form
 				onSubmit={handleSubmit}
 				className='grid grid-cols-1 md:grid-cols-2 gap-6'>
