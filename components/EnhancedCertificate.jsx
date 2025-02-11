1b107df70fc92deb8764b9fd00e6fc9a3aebc169
Arian <Arianbod@outlook.com>
2025-02-11 22:19:32 +0300

    0.5.0.1

--- components/EnhancedCertificate.jsx
+++ components/EnhancedCertificate.jsx
@@ -1,6 +1,6 @@
 // EnhancedCertificate.js
 import React from 'react';
-
+import Data from '@/utils/Data.json';
 const EnhancedCertificate = ({ score, userInput }) => {
 	const getValue = (key) => {
 		const lowerKey = key.toLowerCase();
@@ -9,7 +9,7 @@ const EnhancedCertificate = ({ score, userInput }) => {
 
 	return (
 		<div className='bg-white border-8 border-double border-gray-300 p-8 max-w-2xl mx-auto my-8 text-center'>
-			<h1 className='text-3xl font-bold mb-4'>Deprem Platform</h1>
+			<h1 className='text-3xl font-bold mb-4'>QuakeWise</h1>
 			<h2 className='text-2xl text-blue-600 font-semibold mb-6'>
 				Certificate of Safety and Analysis
 			</h2>
@@ -19,7 +19,7 @@ const EnhancedCertificate = ({ score, userInput }) => {
 				{userInput.location
 					? `Lat: ${userInput.location.latitude}, Lng: ${userInput.location.longitude}`
 					: 'the specified location'}
-				, has been thoroughly analyzed by the Deprem Platform.
+				, has been thoroughly analyzed by the QuakeWise.
 			</p>
 
 			<div className='mb-6'>
@@ -51,7 +51,7 @@ const EnhancedCertificate = ({ score, userInput }) => {
 
 			<p className='mb-6'>
 				This certificate is issued to confirm the safety and structural analysis
-				performed by the Deprem Platform.
+				performed by the QuakeWise.
 			</p>
 
 			<div className='mt-8'>
@@ -64,7 +64,7 @@ const EnhancedCertificate = ({ score, userInput }) => {
 					</div>
 					<div>
 						<div className='border-t-2 border-black pt-2'>
-							Seal of Deprem Platform
+							Seal of QuakeWise
 						</div>
 					</div>
 				</div>
